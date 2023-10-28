bdfa639030cdb470b68329868a5fcfaf0f54af01
radhesh <radheshshetty0407@gmail.com>
2023-10-28 10:21:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const inputbox = document.getElementById("input-box");
+const  listconatiner= document.getElementById("list-container");
+function addtask(){
+    if(inputbox.value === ''){
+        alert("Dont be lazy...do some thing in life");
+
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listconatiner.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    savedata();
+}
+
+listconatiner.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+function savedata(){
+    localStorage.setItem("data",listconatiner.innerHTML);
+}
+function showdata(){
+    listconatiner.innerHTML =localStorage.getItem("data");
+}
+showdata();
